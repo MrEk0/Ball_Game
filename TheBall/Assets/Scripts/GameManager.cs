f0f0d03707de5e3e49a831cb0801628d3f468e14
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-08-14 15:15:37 +0300

    Start/lose panels

---
+++ TheBall/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Play()
+    {
+        //close start panel
+    }
+}
